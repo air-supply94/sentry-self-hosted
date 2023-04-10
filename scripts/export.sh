a2215bcb83a5f6db627c0ac7c311e127c5a5dc19
joey <joey@egenie.cn>
2023-04-10 13:35:18 +0800

    feat(*): 更改postgres配置方式

--- scripts/export.sh
+++ scripts/export.sh
@@ -2,6 +2,6 @@ cd /data/sentry-self-hosted/
 docker compose run --rm -T -e SENTRY_LOG_LEVEL=CRITICAL web export > backup.json
 git add ./backup.json
 git commit -m "chore(*): 备份配置文件"
-git pull origin master
+git pull --rebase origin master
 git push origin master
 
